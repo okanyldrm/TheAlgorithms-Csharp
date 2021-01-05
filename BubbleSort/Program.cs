0a44e1c830bf9b5bb166e1d24edf86052a3df907
okanyldrm <okanyldrm68@gmail.com>
2021-01-05 21:30:54 +0300

    bubblesort done

--- BubbleSort/Program.cs
+++ BubbleSort/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace BubbleSort
 {
@@ -6,7 +8,36 @@ namespace BubbleSort
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            List<int> numbers = new List<int>(){1,4,33,7,11,26,72,71,70,69,55};
+
+            int curr;
+
+            for (int j=0;j<=numbers.Count-2;j++)
+            {
+                for (int i=0; i <= numbers.Count-2;i++)
+                {
+                    if (numbers[i] > numbers[i + 1])
+                    {
+                        curr = numbers[i+1];
+                        numbers[i+1] = numbers[i];
+                        numbers[i] = curr;
+                    }
+                }
+            }
+
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+
+
+
+
+
+
+
+
         }
     }
 }
